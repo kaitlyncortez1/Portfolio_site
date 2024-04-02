5a0e7c72f0657b192597c8c98c9ed9a51311f893
Tim McKenna @ YCP <156977907+TimMcKennaYCP@users.noreply.github.com>
2024-04-02 11:54:26 -0400

    Tim's Updates
    What I updated:
    - Added space in the navbar so it's not touching the right side of the browsers
    - Got the show/hide functionality working
    - Got the spacing updated on about page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Start writing your Javascript
+
+jQuery(function() {
+    jQuery('#showall').click(function() {
+      jQuery('.targetDiv').show();
+    });
+    jQuery('.showSingle').click(function() {
+      jQuery('.targetDiv').hide();
+      jQuery('#work' + $(this).attr('target')).show();
+    });
+  });
